29669456f75cb7248ebf234a136ce2083e8feebe
onava <enava23@yahoo.com>
2021-07-16 01:22:03 -0400

    express dependencies added

---
+++ server.js
@@ -0,0 +1,13 @@
+// Dependencies
+const exp = require('constants');
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
